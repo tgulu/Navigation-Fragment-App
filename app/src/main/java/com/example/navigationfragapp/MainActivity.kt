7c6ce43f26735641aab1db6d4107c38fea4d8b6f
thabogulu <thabogulu@gmail.com>
2022-06-14 20:59:25 +0100

    Adding Nav Frag App code

---
+++ app/src/main/java/com/example/navigationfragapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.navigationfragapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.navigationfragapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_container_fragment)
+        //binding.navigationBar.setupWithNavController(navController)
+    }
+}
